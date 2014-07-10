f13df5016e91785bbbb21a5717f332dfbec87ff1
danzel <danzel@localhost.geek.nz>
2014-07-10 12:00:27 +1200

    Nuspec 1.0.0

--- GeoJsonSharp/Properties/AssemblyInfo.cs
+++ GeoJsonSharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GeoJsonSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Parses GeoJson into NTS Objects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Smartrak")]
 [assembly: AssemblyProduct("GeoJsonSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Smartrak 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
