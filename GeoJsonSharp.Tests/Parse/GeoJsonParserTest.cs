b37e6b08f276d0ec6449d612915b4866da4331ef
Alexey Kosobrukhov <alexey.kosobrukhov@smartrak.com>
2023-04-03 16:33:00 +1000

    [PS-1106] Added parsing (ignoring) of name property.

--- GeoJsonSharp.Tests/Parse/GeoJsonParserTest.cs
+++ GeoJsonSharp.Tests/Parse/GeoJsonParserTest.cs
@@ -1,33 +1,56 @@
 ﻿using System;
+
 using GeoJsonSharp.Parse;
+
 using NUnit.Framework;
 
 namespace GeoJsonSharp.Tests.Parse
 {
-    /// <summary>
-    /// The geo json parser test.
-    /// </summary>
-    public class GeoJsonParserTest
-    {
-        /// <summary>
-        /// The parse feature with date property.
-        /// </summary>
-        [Test]
-        public void ParseFeatureWithDateProperty()
-        {
-            // Arrange
+	/// <summary>
+	/// The geo json parser test.
+	/// </summary>
+	public class GeoJsonParserTest
+	{
+		/// <summary>
+		/// The parse feature with date property.
+		/// </summary>
+		[Test]
+		public void ParseFeatureWithDateProperty()
+		{
+			// Arrange
 
-            // GeoJSON FeatureCollection having one feature
-            // Feature has property - name:'processed' and is of type 'DateTime'
-            var geoJsonString = "{\"type\": \"FeatureCollection\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:EPSG::4283\" } },\"features\": [{\"type\": \"Feature\",\"properties\": {\"title\": \"Feature with date property\",\"processed\": \"2015-08-10T11:10:38-00:00\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [ 152.7465, -31.1015 ]}}]}";
+			// GeoJSON FeatureCollection having one feature
+			// Feature has property - name:'processed' and is of type 'DateTime'
+			var geoJsonString = "{\"type\": \"FeatureCollection\",\"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:EPSG::4283\" } },\"features\": [{\"type\": \"Feature\",\"properties\": {\"title\": \"Feature with date property\",\"processed\": \"2015-08-10T11:10:38-00:00\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [ 152.7465, -31.1015 ]}}]}";
 
-            // Act
-            var geoParser = new GeoJsonParser(geoJsonString, new ParserSettings {SkipInvalidGeometry = false});
-            var featureCollection = geoParser.Parse();
-            var processed = featureCollection.Features[0].Attributes["processed"];
+			// Act
+			var geoParser = new GeoJsonParser(geoJsonString, new ParserSettings { SkipInvalidGeometry = false });
+			var featureCollection = geoParser.Parse();
+			var processed = featureCollection.Features[0].Attributes["processed"];
 
-            // Assert
-            Assert.That(processed, Is.InstanceOf<DateTime>());
-        }
-    }
+			// Assert
+			Assert.That(processed, Is.InstanceOf<DateTime>());
+		}
+
+		/// <summary>
+		/// The parse feature collection with name property.
+		/// </summary>
+		[Test]
+		public void ParseFeatureCollectionWithNameDateProperty()
+		{
+			// Arrange
+
+			// GeoJSON FeatureCollection having one feature
+			// Feature has property - name:'processed' and is of type 'DateTime'
+			var geoJsonString = "{\"type\": \"FeatureCollection\",\"name\": \"SomeName\", \"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:EPSG::4283\" } },\"features\": [{\"type\": \"Feature\",\"properties\": {\"title\": \"Feature with date property\",\"processed\": \"2015-08-10T11:10:38-00:00\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [ 152.7465, -31.1015 ]}}]}";
+
+			// Act
+			var geoParser = new GeoJsonParser(geoJsonString, new ParserSettings { SkipInvalidGeometry = false });
+			var featureCollection = geoParser.Parse();
+			var processed = featureCollection.Features[0].Attributes["processed"];
+
+			// Assert
+			Assert.That(processed, Is.InstanceOf<DateTime>());
+		}
+	}
 }
